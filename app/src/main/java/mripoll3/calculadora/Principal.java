6699aaa1a05a922371a09a18b9ea80c37b2cea3a
“Michael” <michael-ripoll@hotmail.com>
2018-08-16 21:16:31 -0500

    Aplicacion en funcionamiento con validaciones

--- app/src/main/java/mripoll3/calculadora/Principal.java
+++ app/src/main/java/mripoll3/calculadora/Principal.java
@@ -8,6 +8,7 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Principal extends AppCompatActivity {
 
@@ -44,30 +45,63 @@ public class Principal extends AppCompatActivity {
     {
         int opcion;
         double num1, num2, resultado = 0;
-        opcion = operaciones.getSelectedItemPosition();
-        num1 = Double.parseDouble(n1.getText().toString());
-        num2 = Double.parseDouble(n2.getText().toString());
-
-        switch (opcion)
+        if (validar())
         {
-            case 0:
-                resultado = Metodos.sumar(num1, num2);
-                break;
+            opcion = operaciones.getSelectedItemPosition();
+            num1 = Double.parseDouble(n1.getText().toString());
+            num2 = Double.parseDouble(n2.getText().toString());
 
-            case 1:
-                resultado = Metodos.restar(num1, num2);
-                break;
+            switch (opcion) {
+                case 0:
+                    resultado = Metodos.sumar(num1, num2);
+                    break;
 
-            case 2:
-                resultado = Metodos.multiplicar(num1, num2);
-                break;
+                case 1:
+                    resultado = Metodos.restar(num1, num2);
+                    break;
 
-            case 3:
-                resultado = Metodos.dividir(num1, num2);
-                break;
+                case 2:
+                    resultado = Metodos.multiplicar(num1, num2);
+                    break;
+
+                case 3:
+                    resultado = Metodos.dividir(num1, num2);
+                    break;
+            }
+
+            res.setText("" + String.format("%.2f", resultado));
+        }
+    }
+
+    public boolean validar()
+    {
+        int o = operaciones.getSelectedItemPosition();
+        if(n1.getText().toString().isEmpty())
+        {
+            n1.setError(recursos.getString(R.string.error_1));
+            return false;
         }
 
-        res.setText(""+String.format("%.2f",resultado));
+        if(n2.getText().toString().isEmpty())
+        {
+            n2.setError(recursos.getString(R.string.error_2));
+            return false;
+        }
+
+        if(o == 0)
+        {
+            Toast.makeText(this, recursos.getString(R.string.error_3), (Toast.LENGTH_SHORT)).show();
+            return false;
+        }
+
+        if(0 == 4 && Double.parseDouble(n2.getText().toString()) == 0)
+        {
+            n2.setError(recursos.getString(R.string.error_4));
+            n2.requestFocus();
+            return false;
+        }
+
+        return true;
     }
 
     public void limpiar(View v)
@@ -79,3 +113,4 @@ public class Principal extends AppCompatActivity {
         n1.requestFocus();
     }
 }
+
