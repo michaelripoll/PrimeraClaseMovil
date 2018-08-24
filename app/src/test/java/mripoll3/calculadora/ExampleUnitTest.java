195bcff4d0a336c047ae3d97fcff1948677bfe58
“Michael” <michael-ripoll@hotmail.com>
2018-08-23 19:51:28 -0500

    Calculadora testeada

--- app/src/test/java/mripoll3/calculadora/ExampleUnitTest.java
+++ app/src/test/java/mripoll3/calculadora/ExampleUnitTest.java
@@ -14,4 +14,37 @@ public class ExampleUnitTest {
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
     }
+
+    @Test
+    public void sumar_dos_numeros()
+    {
+        double a = 4, b = 6, c;
+        c = 10;
+        assertEquals(c, Metodos.sumar(a,b), 0);
+
+    }
+
+    @Test
+    public void restar_dos_numeros()
+    {
+        double a = 4, b = 6, c;
+        c = -2;
+        assertEquals(c, Metodos.restar(a,b), 0);
+    }
+
+    @Test
+    public void multiplicar_dos_numeros()
+    {
+        double a = 4, b = 6, c;
+        c = 24;
+        assertEquals(c, Metodos.multiplicar(a,b), 0);
+    }
+
+    @Test
+    public void dividir_dos_numeros()
+    {
+        double a = 4, b = 6, c;
+        c = a/b;
+        assertEquals(c, Metodos.dividir(a,b), 0);
+    }
 }
